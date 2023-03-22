d954e9500bee002fd8772e12f21805ffcbe6dfb0
Preston Hall <prestonhall31@gmail.com>
2023-03-21 18:46:50 -0700

    created roomview API

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
+
+# Creating an API view, rather than an HTTP view
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
