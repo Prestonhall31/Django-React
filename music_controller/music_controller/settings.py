89d039cfe7c12fa1e192c9b6bf2aa491921af545
Preston Hall <prestonhall31@gmail.com>
2023-03-22 20:48:31 -0700

    Added React Frontend. Completed testing and confirmed working in dev mode.

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig', # added app to the file from .apps/app.py
     'rest_framework', # 
+    'frontend.apps.FrontendConfig', # added React frontend
 ]
 
 MIDDLEWARE = [
